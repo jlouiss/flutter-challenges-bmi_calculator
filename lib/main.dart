dcea781d191738bfcf6abdecdee267236fc612ab
Jean Louis Borba Rosa Salbego <info@jlouis.me>
2020-08-21 22:09:51 +0100

    :sparkles: Create BmiInputPage widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,37 @@
-void main() {}
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BMICalculator());
+}
+
+class BMICalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: BmiInputPage(),
+    );
+  }
+}
+
+class BmiInputPage extends StatefulWidget {
+  @override
+  _BmiInputPageState createState() => _BmiInputPageState();
+}
+
+class _BmiInputPageState extends State<BmiInputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI Calculator'),
+      ),
+      body: Center(
+        child: Text('Body Text'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
