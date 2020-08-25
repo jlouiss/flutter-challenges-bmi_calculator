9fd9552476f50c90e07f436fc42c30c3f6459844
Jean Louis Borba Rosa Salbego <info@jlouis.me>
2020-08-25 19:40:01 +0100

    :lipstick: Add basic theme

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,16 @@ void main() {
   runApp(BMICalculator());
 }
 
+const mainColor = Color(0xFF0A0E21);
+
 class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: mainColor,
+        scaffoldBackgroundColor: mainColor,
+      ),
       home: BmiInputPage(),
     );
   }
